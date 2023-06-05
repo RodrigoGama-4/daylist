93e9fcea1862b86deeae958eb2ec43a261cfe1c1
Davi Alexandre <deltreertl@gmail.com>
2023-06-05 02:08:01 -0300

    framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { useQuery } from '@apollo/client';
 import { graphql } from '@/graphql/types';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 const GET_DATA = graphql(`
   query Query {
@@ -10,10 +12,17 @@ const GET_DATA = graphql(`
 `);
 
 export default function Home() {
+  const [isVisible, setVisible] = useState(true);
+  const toggleVisibility = () => setVisible(!isVisible);
   const { loading, error, data } = useQuery(GET_DATA);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
+
+  const variants = {
+    aberto: { opacity: 1, x: 0 },
+    fechado: { opacity: 0, x: '-100%' },
+  };
 
   return (
     <main>
@@ -26,6 +35,35 @@ export default function Home() {
           </a>
         </span>
       </p>
+      <div>
+        <motion.nav
+          animate={isVisible ? 'aberto' : 'fechado'}
+          variants={variants}
+          style={{
+            background: '#EEE',
+            width: 300,
+          }}
+          className="nav nav-tabs"
+        >
+          <div>a</div>
+          <div>b</div>
+        </motion.nav>
+        <motion.button
+          className="btn btn-primary"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 1 }}
+          onClick={toggleVisibility}
+          drag
+          dragConstraints={{
+            top: -0,
+            left: -0,
+            right: 100,
+            bottom: 100,
+          }}
+        >
+          Click & Drag
+        </motion.button>
+      </div>
     </main>
   );
 }
